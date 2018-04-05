48bf9276d8d5ff0b496e76adf12d0958a8301de9
horaciocoronel <arielc@gmail.com>
2018-04-05 16:38:33 -0400

    Add models to define Idea object. load the model in app.js

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ mongoose.connect('mongodb://localhost:27017/vidjot')
 	.then(() => console.log('MongoDB connected'))
 	.catch(err => console.log(err));
 
+// Load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
 // Handlebars Middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');

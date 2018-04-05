08df09846f5dd35fb934310151e7dee2f55990cb
horaciocoronel <arielc@gmail.com>
2018-04-05 18:20:28 -0400

    Add edit function, fix error

--- app.js
+++ app.js
@@ -93,7 +93,20 @@ app.post('/ideas', (req, res) => {
 
 // Edit Form process
 app.put('/ideas/:id', (req, res) => {
-	res.send('PUT')
+	Idea.findOne({
+		_id: req.params.id
+	})
+	.then(idea => {
+		console.log(idea);
+		// New Values
+		idea.title = req.body.title;
+		idea.details = req.body.details;
+
+		idea.save()
+			.then(idea => {
+				res.redirect('/ideas');
+			})
+	})
 })
 
 

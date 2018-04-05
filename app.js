4f1a832ab952b734100114da1a3657fe6c02d5d0
horaciocoronel <arielc@gmail.com>
2018-04-05 18:45:21 -0400

    Add Global variables for messages

--- app.js
+++ app.js
@@ -38,6 +38,14 @@ app.use(session({
 // Connect-flash Middleware
 app.use(flash());
 
+// Global Variables
+app.use((req, res, next) => {
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
+	next();
+})
+
 // Routes
 app.get('/', (req, res) => {
 	const title = "Welcome";

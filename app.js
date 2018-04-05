4b7d49832e4b0dd2419d682bed54690ad6fffc8e
horaciocoronel <arielc@gmail.com>
2018-04-05 16:32:43 -0400

    Create connection with mongoose to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+
 const app = express();
+
+// Connect to mongoose
+mongoose.connect('mongodb://localhost:27017/vidjot')
+	.then(() => console.log('MongoDB connected'))
+	.catch(err => console.log(err));
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
